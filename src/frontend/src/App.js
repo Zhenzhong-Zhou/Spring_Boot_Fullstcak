33f262f2a6f292d6c371cfac97c96befd2a07554
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-08-31 02:12:57 -0600

    Fetch and display data in the frontend

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,27 @@
+import {useEffect, useState} from "react";
+import {getAllStudents} from "./client";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => {
+    const [students, setStudents] = useState([]);
+    const fetchStudents = () => {
+        getAllStudents().then(res => res.json()).then(data => setStudents(data));
+    }
 
-    </div>
-  );
+    useEffect(() => {
+        console.log("component is mounted!")
+        fetchStudents();
+    }, []);
+
+    if (students.length <= 0) {
+        return "no data";
+    }
+
+    return (
+        <div>{students.map((student, index) =>
+            <div key={index}>{student.id} {student.name}</div>
+        )}</div>
+    );
 }
 
 export default App;
