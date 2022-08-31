8f1e3a7a0e4f689c7efd21bf77ebd5082a710063
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-08-31 00:28:19 -0600

    Add student model

---
+++ src/main/java/com/zhenzhongzhou/studentmanagement/student/Gender.java
@@ -0,0 +1,5 @@
+package com.zhenzhongzhou.studentmanagement.student;
+
+public enum Gender {
+   MALE, FEMALE, OTHER
+}
---
+++ src/main/java/com/zhenzhongzhou/studentmanagement/student/Student.java
@@ -0,0 +1,16 @@
+package com.zhenzhongzhou.studentmanagement.student;
+
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private Gender gender;
+}
