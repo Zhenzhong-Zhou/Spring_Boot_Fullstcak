988da98ca056d04f627a244d00c5662171b11a03
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-08-31 14:37:51 -0600

    Add side bar component

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,26 +1,9 @@
-import {useEffect, useState} from "react";
-import {getAllStudents} from "./client";
 import './App.css';
+import {SideBar} from "./components";
 
 const App = () => {
-    const [students, setStudents] = useState([]);
-    const fetchStudents = () => {
-        getAllStudents().then(res => res.json()).then(data => setStudents(data));
-    }
-
-    useEffect(() => {
-        console.log("component is mounted!")
-        fetchStudents();
-    }, []);
-
-    if (students.length <= 0) {
-        return "no data";
-    }
-
     return (
-        <div>{students.map((student, index) =>
-            <div key={index}>{student.id} {student.name}</div>
-        )}</div>
+        <SideBar/>
     );
 }
 
---
+++ src/frontend/src/components/SideBar/SideBar.jsx
@@ -0,0 +1,66 @@
+import {Breadcrumb, Layout, Menu} from "antd";
+import {DesktopOutlined, FileOutlined, PieChartOutlined, TeamOutlined, UserOutlined} from "@ant-design/icons";
+import {useEffect, useState} from "react";
+import {getAllStudents} from "../../client";
+import "./styles.css";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+function getItem(label, key, icon, children) {
+    return {key, icon, children, label};
+}
+
+const items = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
+];
+
+const SideBar = () => {
+    const [students, setStudents] = useState([]);
+    const [collapsed, setCollapsed] = useState(false);
+    const fetchStudents = () => {
+        getAllStudents().then(res => res.json()).then(data => setStudents(data));
+    }
+
+    useEffect(() => {
+        console.log("component is mounted!")
+        fetchStudents();
+    }, []);
+
+    if (students.length <= 0) {
+        return "no data";
+    }
+
+    return (
+        <Layout style={{minHeight: '100vh'}}>
+            <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+                <div className="logo" />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+            </Sider>
+            <Layout className="site-layout">
+                <Header className="site-layout-background" style={{padding: 0}}/>
+                <Content style={{margin: '0 16px',}}>
+                    <Breadcrumb style={{margin: '16px 0'}}>
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+                        Bill is a cat.
+                    </div>
+                </Content>
+                <Footer style={{textAlign: 'center'}}>
+                    Ant Design ©2018 Created by Zhenzhong Zhou Server by Java
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+}
+
+export default SideBar;
